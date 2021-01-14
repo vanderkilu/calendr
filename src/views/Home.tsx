de471edfa178190a54ed6b8c4afa703d9a5e4b37
vanderkilu <vanderkiliku@gmail.com>
2021-01-14 23:19:04 +0000

    add task preview modal

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,10 +6,25 @@ import useCalendar from "../hooks/useCalendar";
 import Cell from "../components/Cell";
 import { ID } from "../utils";
 import { CalendarHeaderMonth } from "../components/CalendarHeader";
+import TaskModal from "../components/TaskModal";
+import TaskForm from "../components/TaskCreateForm";
+import TaskPreview from "../components/TaskPreviewForm";
 
 const CellContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(7, 1fr);
+`;
+
+export const CellTask = styled.div`
+  padding: 0.4rem;
+  border-radius: 2px;
+  background-color: #e8f5e9;
+  margin-bottom: 0.2rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  cursor: pointer;
+  overflow: hidden;
 `;
 
 const Home = () => {
@@ -40,6 +55,20 @@ const Home = () => {
           />
         ))}
       </CellContainer>
+      <TaskModal isOpen={false} onClose={() => null} width={25}>
+        {[1, 2, 3, 4, 5].map((i) => (
+          <CellTask>{i}</CellTask>
+        ))}
+      </TaskModal>
+      {/* <TaskForm
+        isOpen={false}
+        onClose={() => null}
+        width={50}
+        onDelete={() => null}
+        onSave={() => null}
+        onEdit={() => null}
+      ></TaskForm> */}
+      <TaskPreview isOpen={true} onClose={() => null} width={30}></TaskPreview>
     </>
   );
 };

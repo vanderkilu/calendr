61494811dc1f36d1228ca06cd9b76eccdd40761d
vanderkilu <vanderkiliku@gmail.com>
2021-01-17 19:59:08 +0000

    update .env validation

--- server/src/utils/validateEnv.ts
+++ server/src/utils/validateEnv.ts
@@ -2,11 +2,7 @@ import { cleanEnv, port, str } from "envalid";
 
 function validateEnv() {
   cleanEnv(process.env, {
-    NODE_ENV: str(),
-    MONGO_USER: str(),
-    MONGO_PASSWORD: str(),
-    MONGO_PATH: str(),
-    MONGO_DATABASE: str(),
+    MONGODB_URI: str(),
     DB_NAME: str(),
     PORT: port(),
   });
